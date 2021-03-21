7843a854061736d1fe24411efcd599e763fc27bb
anis_6883 <anisseam238@gmail.com>
2021-03-21 16:41:10 +0600

    Make Changes

--- Blog_Project/settings.py
+++ Blog_Project/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' #new
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL  = '/media/'
